d8a9c608d50aad103a0714e92bc01aa8db89fdf7
zeltbrennt <zeltbrennt@gmail.com>
2023-05-09 08:46:51 +0200

    Solve Part 2

--- day01.py
+++ day01.py
@@ -1,5 +1,4 @@
 
-
 def part1(puzzle):
     answer = int(puzzle[0]) if puzzle[0] == puzzle[-1] else 0
     prev = puzzle[0]
@@ -7,6 +6,25 @@ def part1(puzzle):
         if digit == prev: answer += int(digit)
         prev = digit
     return answer
-    
+
+assert part1("1122") == 3
+assert part1("1111") == 4
+assert part1("1234") == 0
+assert part1("91212129") == 9
+
 print(f"Part1: {part1(puzzle)}")
 
+def part2(puzzle):
+    answer = 0
+    offset = len(puzzle) // 2
+    for i, d in enumerate(puzzle):
+        if d == puzzle[(i + offset) % len(puzzle)]: answer += int(d)
+    return answer
+
+assert part2("1212") == 6
+assert part2("1221") == 0
+assert part2("123425") == 4
+assert part2("123123") == 12
+assert part2("12131415") == 4
+
+print(f"Part2: {part2(puzzle)}")
